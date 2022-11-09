579ca8d05f7cc613e7b8414c13020cac5ce72f84
Ali Saidi <ali.saidi@esprit.tn>
2022-11-10 00:21:37 +0100

    test mock

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO stock (id_stock, libelle_stock,qte,qte_min) VALUES (1, 'test1',10,100);
-INSERT INTO stock (id_stock, libelle_stock,qte,qte_min) VALUES (2, 'test2',20,200);
+INSERT INTO stocks (id_stock, libelle_stock,qte,qte_min) VALUES (1, 'test1',10,100);
+INSERT INTO stocks (id_stock, libelle_stock,qte,qte_min) VALUES (2, 'test2',20,200);
 
