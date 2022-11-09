0277f9f83e1dee2fbc2a5c53a1e46f69d9c1bd7a
Ali Saidi <ali.saidi@esprit.tn>
2022-11-09 18:58:27 +0100

    test correction

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -2,14 +2,14 @@ package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
 import java.util.List;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class StockServiceImplTest {
 	@Autowired
