bffd0d1b740889abed661f0e8e0ac29c77dd333b
Ali Saidi <ali.saidi@esprit.tn>
2022-11-09 19:34:49 +0100

    profile

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,7 +1,8 @@
 package com.esprit.examen.services;
 
-import static org.junit.Assert.*;
+
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 //import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
