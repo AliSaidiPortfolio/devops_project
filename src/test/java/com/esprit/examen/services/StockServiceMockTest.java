282e0937a3bf10be0f77354f3ae5f3a389ddd733
Ali Saidi <ali.saidi@esprit.tn>
2022-11-10 02:27:44 +0100

    test junit

---
+++ src/test/java/com/esprit/examen/services/StockServiceMockTest.java
@@ -0,0 +1,81 @@
+package com.esprit.examen.services;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.esprit.examen.repositories.StockRepository;
+import org.hibernate.type.TrueFalseType;
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+import com.esprit.examen.entities.Stock;
+@SpringBootTest
+public class StockServiceMockTest {
+
+    @Mock
+    StockRepository stockRepository;
+    @InjectMocks
+    StockServiceImpl stockService;
+    //mock assure le type de retour qu on veut par la methode
+    @Test
+    public void testRetrieveStockById(){
+
+
+        Stock s =new Stock("test stock",70,700);
+        Mockito.when(stockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(s));
+        assertNotNull(stockService.retrieveStock(1L));
+
+    }
+    @Test
+    public void testRetrieveAllStock()  {
+        Stock s =new Stock("test stock",70,700);
+        List<Stock> stocks = new ArrayList<>();
+        stocks.add(s);
+        Mockito.when(stockService.retrieveAllStocks()).thenReturn(stocks);
+        List<Stock> result = stockService.retrieveAllStocks();
+        assertEquals(result.size(), 1);
+    }
+
+    @Test
+    public void testAddStock(){
+
+
+        Stock s =new Stock("test stock",70,700);
+        Mockito.doNothing().when(stockService).addStock(s);
+        stockService.addStock(s);
+        assertTrue(true);
+    }
+
+    @Test
+    public void testUpdateStock(){
+
+
+        Stock s =new Stock("test stock",70,700);
+        Mockito.doNothing().when(stockService).updateStock(s);
+        stockService.updateStock(s);
+        assertTrue(true);
+    }
+    @Test
+    public void testDeleteStock()  {
+        Mockito.doNothing().when(stockService).deleteStock(1l);
+        stockService.deleteStock(1l);
+        assertTrue(true);
+    }
+
+
+
+
+
+
+
+
+
+}
