7b4d0132b3ba428b018498e075f7d92ce3f911b8
Ali Saidi <ali.saidi@esprit.tn>
2022-11-10 01:13:39 +0100

    test mock

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -28,9 +28,9 @@ public class StockServiceImplTest {
 
 	@Test
 	public void testAddStock() {
-		//      List<Stock> stocks = stockService.retrieveAllStocks();
-		//      int expected=stocks.size();
-		Stock s = new Stock("stock test",10,100);
+//		List<Stock> stocks = stockService.retrieveAllStocks();
+//		      int expected=stocks.size();
+		Stock s = new Stock("stock test",600,60);
 		Stock savedStock= stockService.addStock(s);
 
 		//      assertEquals(expected+1, stockService.retrieveAllStocks().size());
@@ -40,20 +40,35 @@ public class StockServiceImplTest {
 	}
 
 	@Test
+	public void testRetrieveStockById() {
+
+		Stock s = new Stock("stock test",600,60);
+		Stock savedStock= stockService.addStock(s);
+		Stock updateStock=stockService.updateStock(new Stock("stock test",700,70));
+//		assertNotNull(savedStock.getIdStock());
+		assertSame(updateStock.getIdStock(), savedStock.getIdStock());
+		assertSame(700, updateStock.getQte());
+
+//		assertTrue(savedStock.getQteMin()>0);
+		stockService.deleteStock(savedStock.getIdStock());
+
+	}
+	@Test
 	public void testAddStockOptimized() {
 
-		Stock s = new Stock("stock test",10,100);
+		Stock s = new Stock("stock test",600,60);
 		Stock savedStock= stockService.addStock(s);
 		assertNotNull(savedStock.getIdStock());
-		assertSame(10, savedStock.getQte());
+		assertSame(600, savedStock.getQte());
 		assertTrue(savedStock.getQteMin()>0);
 		stockService.deleteStock(savedStock.getIdStock());
 
 	}
 
+
 	@Test
 	public void testDeleteStock() {
-		Stock s = new Stock("stock test",30,60);
+		Stock s = new Stock("stock test",600,60);
 		Stock savedStock= stockService.addStock(s);
 		stockService.deleteStock(savedStock.getIdStock());
 		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
